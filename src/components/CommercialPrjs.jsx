b1046f9bae96bafa78690b0f594ee73e971682e3
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-06 00:11:18 +0100

    Created commercial projects section

---
+++ src/components/CommercialPrjs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CommercialProjects() {
+
+  return (
+    <div>
+      <h2>
+        Commercial Projects
+      </h2>
+    </div>
+  );
+}
+
+export default CommercialProjects;
