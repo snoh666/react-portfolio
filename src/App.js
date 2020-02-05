db7a3e34f49a1e264a6eab76ffce8050823c3fb0
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-06 00:15:52 +0100

    Linked all new components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import store from './redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Welcome from './components/Welcome';
+import About from './components/About';
+import CommercialProjects from './components/CommercialPrjs';
+import GitProjects from './components/GitProjects';
 
 function App() {
   return (
@@ -16,8 +20,20 @@ function App() {
             <section>
               <Welcome />
             </section>
+            <section>
+              <About />
+            </section>
+            <section>
+              <CommercialProjects />
+            </section>
+            <section>
+              <GitProjects />
+            </section>
           </Route>
         </Switch>
+        <footer>
+          <Footer />
+        </footer>
       </Router>
     </Provider>
   );
