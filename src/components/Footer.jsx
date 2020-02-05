fd32c000d0a639a7c3a9e99bb455d3077ed945ca
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-06 00:13:04 +0100

    Created basic footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer() {
+
+  return (
+    <div>
+      <h2>
+        Footer
+      </h2>
+
+      <div>
+        {
+          // Output here socials links and etc
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
