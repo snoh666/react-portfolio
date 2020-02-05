1b6ea1fec981af87dead2e6db8efe81c55a8b62c
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-05 23:43:02 +0100

    Added Header component to every site

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import store from './redux';
 import { Provider } from 'react-redux';
-import { Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
+        <Header />
         <Switch>
           <Route path="/">
 
