1ba57203bbc7faaa3a301f44e62910e29a98e32e
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-05 23:50:48 +0100

    Created section for githubprojects to appear

---
+++ src/components/GitProjects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function GitProjects() {
+
+  return (
+    <div>
+      {
+        // Here goes map and etc functions
+      }
+    </div>
+  )
+}
+
+export default GitProjects;
