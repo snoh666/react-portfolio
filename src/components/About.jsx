878b3c5ac739f304869082450ec729f14a0cf04d
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-06 00:09:52 +0100

    Created about section component

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function About() {
+
+  return (
+    <div>
+      <h2>
+        About me section
+      </h2>
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae expedita corporis fugiat. Unde esse quaerat saepe tempore, minus doloremque sunt nostrum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
