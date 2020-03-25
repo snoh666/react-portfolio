82605c2e7996ff5b70784f99778e66e92a3f287e
Mikolaj Snochowski <snochopl@gmail.com>
2020-03-25 16:59:50 +0100

    Deleted import of unnecessary UnderlinedSpan

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import UnderlinedSpan from './styled/UnderlinedSpan';
 import SectionTitle from './styled/SectionTitle';
 
 function About() {
