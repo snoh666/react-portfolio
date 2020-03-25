d314217cc3ca031a04fbb9aa30e9482cb5d65f99
Mikolaj Snochowski <snochopl@gmail.com>
2020-03-25 15:50:06 +0100

    Mvp of header navigation structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import StyledButton from './styled/StyledButton';
 
 const Header = () => {
 
+  const logo = 'Snoh Co.';
+
   return (
     <StyledHeader>
-      <p>
-        Hello world with styled header
-      </p>
+      <StyledLogo>
+        <h2>
+          {logo}
+        </h2>
+      </StyledLogo>
+      <StyledList>
+        <li>
+          <StyledButton><span>About</span></StyledButton>
+        </li>
+        <li>
+          <StyledButton><span>Offers</span></StyledButton>
+        </li>
+        <li>
+          <StyledButton><span>Contact</span></StyledButton>
+        </li>
+      </StyledList>
     </StyledHeader>
   );
 }
@@ -17,7 +33,37 @@ export default Header;
 
 
 const StyledHeader = styled.header`
-  width: 100vw;
+  width: 100%;
   height: 50px;
+  padding: 0 5%;
+
   position: sticky;
+  display: flex;
+
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+
+  h2 {
+    font-weight: 300;
+  }
+`;
+
+const StyledLogo = styled.div`
+  width: 20%;
+  max-width: 50%;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  min-width: 30%;
+  max-width: 50%;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+
+  li {
+    margin: 0 15px;
+  }
 `;
