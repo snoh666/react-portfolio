c6341807bc0b19a19b5a551a4fe062b5c229dde2
Mikolaj Snochowski <snochopl@gmail.com>
2020-03-29 20:43:46 +0200

    Cleared whole redux container

--- src/redux/actions/index.js
+++
@@ -1,4 +0,0 @@
-const applyAPIResult = state => ({
-  type: 'FETCH_GITHUB_API',
-  apiResult: state
-});
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,14 +1,14 @@
 const initialState = {
-  githubAPI: []
+  sections: {
+    top: false,
+    about: false,
+    projects: false,
+    contact: false
+  }
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_GITHUB_API':
-      return {
-        ...state,
-        githubAPI: action.apiResult
-      };
     default:
       return {
         ...state
