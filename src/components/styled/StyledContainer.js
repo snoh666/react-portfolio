2afba7efc9e78f106577950dec812df82de39f5a
Mikolaj Snochowski <snochopl@gmail.com>
2020-04-05 03:22:28 +0200

    Added StyledContainer for wrapping each component

---
+++ src/components/styled/StyledContainer.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  max-width: 1160px;
+  width: 100%;
+  min-height: 650px;
+  margin: 40px auto;
+  padding: 30px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-content: center;
+  justify-content: center;
+`;
+
+export default StyledContainer;
