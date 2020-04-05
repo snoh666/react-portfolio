c50a0c6ec0bbfe6b66302d9b5a404bffd157fd4c
Mikolaj Snochowski <snochopl@gmail.com>
2020-04-05 03:15:58 +0200

    Upgraded Abput secton with new styled component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
+import StyledContainer from './styled/StyledContainer';
 import SectionTitle from './styled/SectionTitle';
 import { connect } from 'react-redux';
 import { setAboutRef } from '../redux/actions';
@@ -12,11 +13,11 @@ function About({ setAboutRef }) {
   });
 
   return (
-    <AboutWrapper ref={aboutRef}>
+    <StyledContainer ref={aboutRef}>
       <SectionTitle>
         <span>About me</span>
       </SectionTitle>
-      <div>
+      <AboutDescription>
         <p>
           I'm a 19 years old front-end developer with big ambitions. Half a year
           ago I started freelancing with development of responsive Wordpress
@@ -24,24 +25,18 @@ function About({ setAboutRef }) {
           new ones with React and Vue. In the near future I’m planning to get a
           full time job as I’m ending my school in May.
         </p>
-      </div>
-    </AboutWrapper>
+      </AboutDescription>
+    </StyledContainer>
   );
 }
 
 export default connect(null, { setAboutRef })(About);
 
-const AboutWrapper = styled.div`
-  width: 100%;
-  max-width: 760px;
-  min-height: 75vh;
-  margin: 100px auto;
+const AboutDescription = styled.div`
+  max-width: 870px;
+  padding: 0 15px;
+  margin: 15px auto;
   text-align: center;
-
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  justify-content: center;
 
   p {
     line-height: 1.25em;
