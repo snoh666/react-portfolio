3ff766e8fdc25ff4535422ea5581836f61535ff5
Mikolaj Snochowski <snochopl@gmail.com>
2020-04-13 20:58:41 +0200

    Added projects content into object for mapping

---
+++ src/components/site-content/projects.jsx
@@ -0,0 +1,52 @@
+const projects = [
+  {
+    title: 'Marek Belka',
+    links: {
+      site: 'http://marekbelka.com/',
+      code: false,
+    },
+    descrption: `One of first commercial projects i've made. It was possible cause of contacts gained throught friends and etc. Maybe desing and etc arent that much great but I'm really proud of this site by being my first commercial project.`,
+  },
+  {
+    title: 'Generator Pomysłów',
+    links: {
+      site: 'https://generatorpomyslow.pl/',
+      code: false,
+    },
+    description: `I made subpage template and new menu with GSAP for new trainings campain while working with already coded home page and etc. Menu was based on provided by client site with menu which he liked fore reference`,
+  },
+  {
+    title: 'Magazyn Bieganie',
+    links: {
+      site: 'https://www.magazynbieganie.pl/',
+      code: false,
+    },
+    descirption: `Created whole layout from scratch with psd file. Site was preapred for a lot of user per month`,
+  },
+  {
+    title: 'Tellonym with React',
+    links: {
+      site: false,
+      code: 'https://github.com/snoh666/anon-ask-react',
+    },
+    descirption: `Clone of tellonym created with React and headless firebase database storage. Planned to use as replacement for tellonym with spotted fan page.`,
+  },
+  {
+    title: 'Sleep cycle calculator',
+    links: {
+      site: 'https://snoh666.github.io/sleep-cycle-calculator/',
+      code: 'https://github.com/snoh666/sleep-cycle-calculator',
+    },
+    descirption: `Faster and rebuilded in react version of original site. I was using it and had some things i would done in other way so there it is. In my opinion faster and easier in use.`,
+  },
+  {
+    title: 'E Diary - School',
+    links: {
+      site: false,
+      code: 'https://github.com/snoh666/sleep-cycle-calculator',
+    },
+    descirption: `Created as class project in my school, created over skills and level required in class. REST API written with php. Of course there are some mistakes, that i would change but with lack of time i cant for now.`,
+  },
+];
+
+export default projects;
